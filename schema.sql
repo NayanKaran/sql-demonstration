b2cbfb7c9b44dc1c3d177274c04ffb8f73281bec
Debabrata Mandal <bapidebabrata@gmail.com>
2022-08-10 15:29:04 +0530

    Update table schema to automatically generate ID

--- schema.sql
+++ schema.sql
@@ -1,10 +1,13 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals(
-id integer PRIMARY KEY NOT NULL,
+id SERIAL PRIMARY KEY,
 name text NOT NULL,
 date_of_birth date NOT NULL,
 escape_attempts integer,
 neutered boolean,
 weight_kg decimal NOT NULL
 );
+
+ALTER TABLE animals 
+ADD species text;
