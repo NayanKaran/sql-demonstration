94272ce537331f90f9d4eed9a1d6e5e50f0dc481
Debabrata Mandal <bapidebabrata@gmail.com>
2022-08-10 15:29:16 +0530

    Add data to the table

--- data.sql
+++ data.sql
@@ -1,8 +1,15 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES
-(1, 'Agumon', '2020-02-03', 0, TRUE, 10.23),
-(2, 'Gabumon', '2018-11-15', 2, TRUE, 8),
-(3, 'Pikachu', '2021-01-07', 1, FALSE, 15.04),
-(4, 'Devimon', '2017-05-12', 5, TRUE, 11);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES 
+('Agumon', 'February 3, 2020', 0, TRUE, 10.23),
+('Gabumon', 'November 15, 2018', 2, TRUE, 8),
+('Pikachu', 'January 7, 2021', 1, FALSE, 15.04),
+('Devimon', 'May 12, 2017', 5, TRUE, 11),
+('Charmander', 'February 8, 2020', 0, TRUE, 11),
+('Plantmon', 'November 15, 2021', 2, TRUE, 5.7),
+('Squirtle', 'April 2, 1993', 3, TRUE, 12.13),
+('Angemon', 'June 12, 2005', 1, FALSE, 45),
+('Boarmon', 'June 7, 2005', 7, TRUE, 20.4),
+('Blossom', 'October 13, 1998', 3, TRUE, 17),
+('Ditto', 'May 14, 2022', 4, TRUE, 22);
